06f84c8fd616f14aac70d34ebcfa60a5abab34e0
Allan0423 <zhpeng423.cn@gmail.com>
2019-08-26 22:26:56 +0800

    Add fields of Blogger

--- src/main/java/info/enjoycoding/myblog/model/Blogger.java
+++ src/main/java/info/enjoycoding/myblog/model/Blogger.java
@@ -5,14 +5,9 @@ public class Blogger {
     private String bloggerName;
     private String bloggerPwd;
     private String email;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String profile;
+    private String signature;
+    private String profilePicName;
 
     public String getBloggerName() {
         return bloggerName;
@@ -28,5 +23,37 @@ public class Blogger {
 
     public void setBloggerPwd(String bloggerPwd) {
         this.bloggerPwd = bloggerPwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getProfilePicName() {
+        return profilePicName;
+    }
+
+    public void setProfilePicName(String profilePicName) {
+        this.profilePicName = profilePicName;
     }
 }
