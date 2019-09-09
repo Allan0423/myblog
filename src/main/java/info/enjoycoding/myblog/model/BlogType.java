b71fff8d038851ca6c129599548711157dbd6aa0
Allan0423 <zhpeng423.cn@gmail.com>
2019-09-09 23:40:29 +0800

    Add model blogtype

---
+++ src/main/java/info/enjoycoding/myblog/model/BlogType.java
@@ -0,0 +1,56 @@
+package info.enjoycoding.myblog.model;
+
+public class BlogType {
+
+    /**
+     * 类别ID
+     */
+    private Integer id;
+
+    /**
+     * 类别名称
+     */
+    private String name;
+
+    /**
+     * 排序序号
+     */
+    private Integer orderNo;
+
+    /**
+     * 该类别下博客数量
+     */
+    private Integer blogCount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Integer orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public Integer getBlogCount() {
+        return blogCount;
+    }
+
+    public void setBlogCount(Integer blogCount) {
+        this.blogCount = blogCount;
+    }
+}
