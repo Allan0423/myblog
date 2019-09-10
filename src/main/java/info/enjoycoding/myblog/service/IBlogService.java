b973503d347737ab87594c456ea512be2512b89e
Allan0423 <zhpeng423.cn@gmail.com>
2019-09-10 21:03:39 +0800

    Add blog service interface

---
+++ src/main/java/info/enjoycoding/myblog/service/IBlogService.java
@@ -0,0 +1,21 @@
+package info.enjoycoding.myblog.service;
+
+import info.enjoycoding.myblog.model.Blog;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IBlogService {
+
+    List<Blog> list();
+
+    Integer getCount(Map<String, Object> map);
+
+    Blog findById(Integer id);
+
+    Integer addBlog(Blog blog);
+
+    Integer updateBlog(Blog blog);
+
+    Integer deleteBlog(Integer id);
+}
