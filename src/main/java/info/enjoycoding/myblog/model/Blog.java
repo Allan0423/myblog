fe51e555611ccef0f6b485416dc5adca725aff5a
Allan0423 <zhpeng423.cn@gmail.com>
2019-09-10 20:26:48 +0800

    Add blog model

---
+++ src/main/java/info/enjoycoding/myblog/model/Blog.java
@@ -0,0 +1,97 @@
+package info.enjoycoding.myblog.model;
+
+/**
+ * 博客模型
+ */
+public class Blog {
+    /**
+     * 博客id
+     */
+    Integer id;
+
+    /**
+     * 博客类型id
+     */
+    Integer blogTypeId;
+
+    /**
+     * 标题
+     */
+    String title;
+
+    /**
+     * 摘要
+     */
+    String digest;
+
+    /**
+     * 内容
+     */
+    String content;
+
+    /**
+     * 关键字，逗号分隔的词组
+     */
+    String keywords;
+
+    /**
+     * 阅读量
+     */
+    Integer readCount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getBlogTypeId() {
+        return blogTypeId;
+    }
+
+    public void setBlogTypeId(Integer blogTypeId) {
+        this.blogTypeId = blogTypeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDigest() {
+        return digest;
+    }
+
+    public void setDigest(String digest) {
+        this.digest = digest;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public Integer getReadCount() {
+        return readCount;
+    }
+
+    public void setReadCount(Integer readCount) {
+        this.readCount = readCount;
+    }
+}
