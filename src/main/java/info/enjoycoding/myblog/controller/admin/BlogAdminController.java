acecc328b158d3f1eaf70cb89708db8f0270862a
Allan0423 <zhpeng423.cn@gmail.com>
2019-09-17 21:32:41 +0800

    Add some pages

--- src/main/java/info/enjoycoding/myblog/controller/admin/BlogAdminController.java
+++ src/main/java/info/enjoycoding/myblog/controller/admin/BlogAdminController.java
@@ -1,11 +1,17 @@
 package info.enjoycoding.myblog.controller.admin;
 
+import info.enjoycoding.myblog.service.IBloggerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("/admin/blog")
 public class BlogAdminController {
+
+    @Resource
+    private IBloggerService bloggerService;
 
     @RequestMapping("/list")
     public void list(){
